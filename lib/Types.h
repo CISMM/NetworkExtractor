31bc20fc001d753b6d8f407fce49419faa6dbb3b
Cory Quammen <cquammen@cs.unc.edu>
2009-03-15 19:56:33 +0000

    2009-03-15  Cory Quammen <cquammen@cs.unc.edu>
       * Types.h: Added 3D float image type.

--- lib/Types.h
+++ lib/Types.h
@@ -5,10 +5,16 @@
 #include "itkImageFileReader.h"
 #include "itkVTKImageExport.h"
 
-typedef unsigned short UShortPixelType;
 static const unsigned int     Dimension3 = 3;
+
+typedef unsigned short UShortPixelType;
 typedef itk::Image<UShortPixelType, Dimension3> UShort3DImageType;
 typedef itk::ImageFileReader<UShort3DImageType> UShort3DImageReaderType;
 typedef itk::VTKImageExport<UShort3DImageType> UShort3DExporterType;
+
+typedef unsigned short FloatPixelType;
+typedef itk::Image<FloatPixelType, Dimension3> Float3DImageType;
+typedef itk::ImageFileReader<Float3DImageType> Float3DImageReaderType;
+typedef itk::VTKImageExport<Float3DImageType> Float3DExporterType;
 
 #endif // _TYPES_H_
