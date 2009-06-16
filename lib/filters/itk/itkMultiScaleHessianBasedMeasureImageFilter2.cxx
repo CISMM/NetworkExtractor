6bc7b91e7fe2ce76ecef68a0ed9399d482978b09
Cory Quammen <cquammen@cs.unc.edu>
2009-06-16 18:55:11 +0000

    2009-06-16  Cory Quammen <cquammen@cs.unc.edu>
       * itk/itkMultiScaleHessianBasedMeasureImageFilter2.h,.cxx:
       Copied this class from Review directory of current ITK release
       (3.12) and added the generation of an image containing the
       eigenvectors corresponding to the eigenvalue with the minimum
       absolute value.  These vectors are aligned with fiber-like objects
       in the image. This approach is more memory efficient than generating
       the Hessian image, which the class already supported, followed by
       eigenanalysis.
    
       * CMakeLists.txt: Added the above file.

---
+++ lib/filters/itk/itkMultiScaleHessianBasedMeasureImageFilter2.cxx
@@ -0,0 +1,338 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkMultiScaleHessianBasedMeasureImageFilter2.cxx,v $
+  Language:  C++
+  Date:      $Date: 2009/06/16 18:55:11 $
+  Version:   $Revision: 1.1 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkMultiScaleHessianBasedMeasureImageFilter2_cxx
+#define __itkMultiScaleHessianBasedMeasureImageFilter2_cxx
+
+#include "itkMultiScaleHessianBasedMeasureImageFilter2.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageRegionConstIterator.h"
+#include "vnl/vnl_math.h"
+
+namespace itk
+{
+
+/**
+ * Constructor
+ */
+template <typename TInputImage,
+          typename THessianImage, 
+          typename TEigenvectorImage,
+          typename TOutputImage >
+MultiScaleHessianBasedMeasureImageFilter2
+<TInputImage,THessianImage,TEigenvectorImage,TOutputImage>
+::MultiScaleHessianBasedMeasureImageFilter2()
+{
+  m_SigmaMinimum = 0.2;
+  m_SigmaMaximum = 2.0;
+
+  m_NumberOfSigmaSteps = 10;
+  m_SigmaStepMethod = Self::LogarithmicSigmaSteps;
+
+  m_HessianFilter = HessianFilterType::New();
+  m_HessianToMeasureFilter = NULL;
+
+  //Instantiate Update buffer
+  m_UpdateBuffer = UpdateBufferType::New();
+
+  m_GenerateScalesOutput = false;
+  m_GenerateHessianOutput = false;
+  m_GenerateEigenvectorOutput = false;
+
+  typename OutputImageType::Pointer scalesImage = OutputImageType::New();
+  typename HessianImageType::Pointer hessianImage = HessianImageType::New();
+  typename EigenvectorImageType::Pointer eigenvectorImage = EigenvectorImageType::New();
+  this->ProcessObject::SetNumberOfRequiredOutputs(3);
+  this->ProcessObject::SetNthOutput(1,scalesImage.GetPointer());
+  this->ProcessObject::SetNthOutput(2,hessianImage.GetPointer());
+  this->ProcessObject::SetNthOutput(3,eigenvectorImage.GetPointer());
+}
+
+template <typename TInputImage,
+          typename THessianImage, 
+          typename TEigenvectorImage,
+          typename TOutputImage >
+void
+MultiScaleHessianBasedMeasureImageFilter2
+<TInputImage,THessianImage,TEigenvectorImage,TOutputImage>
+::AllocateUpdateBuffer()
+{
+  /* The update buffer looks just like the output and holds the best response
+     in the  objectness measure */
+
+  typename TOutputImage::Pointer output = this->GetOutput();
+
+  m_UpdateBuffer->SetSpacing(output->GetSpacing());
+  m_UpdateBuffer->SetOrigin(output->GetOrigin());
+  m_UpdateBuffer->SetLargestPossibleRegion(output->GetLargestPossibleRegion());
+  m_UpdateBuffer->SetRequestedRegion(output->GetRequestedRegion());
+  m_UpdateBuffer->SetBufferedRegion(output->GetBufferedRegion());
+  m_UpdateBuffer->Allocate();
+
+  // Update buffer is used for > comparisons so make it really really small,
+  // just to be sure. Thanks to Hauke Heibel.
+  m_UpdateBuffer->FillBuffer( itk::NumericTraits< BufferValueType >::NonpositiveMin() );
+}
+
+template <typename TInputImage,
+          typename THessianImage, 
+          typename TEigenvectorImage,
+          typename TOutputImage >
+void
+MultiScaleHessianBasedMeasureImageFilter2
+<TInputImage,THessianImage,TEigenvectorImage,TOutputImage>
+::GenerateData()
+{
+  // Allocate the output
+  this->GetOutput(0)->SetBufferedRegion(this->GetOutput(0)->GetRequestedRegion());
+  this->GetOutput(0)->Allocate();
+
+  if( m_HessianToMeasureFilter.IsNull() )
+    {
+    itkExceptionMacro( " HessianToMeasure filter is not set. Use SetHessianToMeasureFilter() " );
+    }
+
+  if (m_GenerateScalesOutput)
+    {
+    this->GetOutput(1)->SetBufferedRegion(this->GetOutput(1)->GetRequestedRegion());
+    this->GetOutput(1)->Allocate();
+    this->GetOutput(1)->FillBuffer(0);
+    }
+
+  if (m_GenerateHessianOutput)
+    {
+    this->GetOutput(2)->SetBufferedRegion(this->GetOutput(2)->GetRequestedRegion());
+    this->GetOutput(2)->Allocate();
+    }
+
+  if (m_GenerateEigenvectorOutput)
+    {
+    this->GetOutput(3)->SetBufferedRegion(this->GetOutput(3)->GetRequestedRegion());
+    this->GetOutput(3)->Allocate();
+    }
+
+  // Allocate the buffer
+  AllocateUpdateBuffer();
+
+  typename InputImageType::ConstPointer input = this->GetInput();
+
+  this->m_HessianFilter->SetInput(input);
+
+  this->m_HessianFilter->SetNormalizeAcrossScale(true);
+
+  double sigma = m_SigmaMinimum;
+
+  int scaleLevel = 1;
+
+  while (sigma <= m_SigmaMaximum)
+    {
+    std::cout << "Computing measure for scale with sigma = "
+              << sigma << std::endl;
+
+    m_HessianFilter->SetSigma( sigma );
+
+    m_HessianToMeasureFilter->SetInput ( m_HessianFilter->GetOutput() );
+
+    m_HessianToMeasureFilter->Update();
+
+    this->UpdateMaximumResponse(sigma);
+
+    sigma  = this->ComputeSigmaValue( scaleLevel );
+
+    scaleLevel++;
+    }
+
+  //Write out the best response to the output image
+  ImageRegionIterator<UpdateBufferType> it(m_UpdateBuffer,m_UpdateBuffer->GetLargestPossibleRegion());
+  it.GoToBegin();
+
+  ImageRegionIterator<TOutputImage> oit(this->GetOutput(0),this->GetOutput(0)->GetLargestPossibleRegion());
+  oit.GoToBegin();
+
+  while(!oit.IsAtEnd())
+    {
+    oit.Value() = static_cast< OutputPixelType >( it.Get() );
+    ++oit;
+    ++it;
+    }
+}
+
+template <typename TInputImage,
+          typename THessianImage, 
+          typename TEigenvectorImage,
+          typename TOutputImage >
+void
+MultiScaleHessianBasedMeasureImageFilter2
+<TInputImage,THessianImage,TEigenvectorImage,TOutputImage>
+::UpdateMaximumResponse(double sigma)
+{
+  ImageRegionIterator<UpdateBufferType> oit(m_UpdateBuffer,m_UpdateBuffer->GetLargestPossibleRegion());
+
+  typename OutputImageType::Pointer scalesImage = static_cast<OutputImageType*>(this->ProcessObject::GetOutput(1));
+  ImageRegionIterator<OutputImageType> osit(scalesImage,scalesImage->GetLargestPossibleRegion());
+
+  typename HessianImageType::Pointer hessianImage = static_cast<HessianImageType*>(this->ProcessObject::GetOutput(2));
+  ImageRegionIterator<HessianImageType> ohit(hessianImage,hessianImage->GetLargestPossibleRegion());
+
+  typename EigenvectorImageType::Pointer eigenvectorImage = static_cast<EigenvectorImageType*>(this->ProcessObject::GetOutput(3));
+  ImageRegionIterator<EigenvectorImageType> oeit(eigenvectorImage,eigenvectorImage->GetLargestPossibleRegion());
+
+  oit.GoToBegin();
+  if (m_GenerateScalesOutput)
+    {
+    osit.GoToBegin();
+    }
+  if (m_GenerateHessianOutput)
+    {
+    ohit.GoToBegin();
+    }
+  if (m_GenerateEigenvectorOutput)
+    {
+    oeit.GoToBegin();
+    }
+
+  typedef typename HessianToMeasureFilterType::OutputImageType HessianToMeasureOutputImageType;
+
+  ImageRegionIterator<HessianToMeasureOutputImageType> it(m_HessianToMeasureFilter->GetOutput(),
+    this->m_HessianToMeasureFilter->GetOutput()->GetLargestPossibleRegion());
+  ImageRegionIterator<HessianImageType> hit(m_HessianFilter->GetOutput(),
+    this->m_HessianFilter->GetOutput()->GetLargestPossibleRegion());
+
+  it.GoToBegin();
+  hit.GoToBegin();
+
+  while(!oit.IsAtEnd())
+    {
+    if( oit.Value() < it.Value() )
+      {
+      oit.Value() = it.Value();
+      if (m_GenerateScalesOutput)
+        {
+        osit.Value() = static_cast< OutputPixelType >( sigma );
+        }
+      if (m_GenerateHessianOutput)
+        {
+        ohit.Value() = hit.Value();
+        }
+      if (m_GenerateEigenvectorOutput)
+      {
+        typename EigenvectorImageType::PixelType eigenvalues; // Borrow the eigenvector type for this
+        Matrix<typename EigenvectorImageType::PixelType::ValueType, Self::ImageDimension, Self::ImageDimension> eigenvectors;
+        
+        // Perform the eigenanalysis on the Hessian matrix
+        hit.Value().ComputeEigenAnalysis(eigenvalues, eigenvectors);
+
+        // Find the eigenvalue with the smallest absolute value
+        int smallestAbsEigenvalueIndex = 0;
+        if (fabs(eigenvalues[1]) < fabs(eigenvalues[smallestAbsEigenvalueIndex]))
+          smallestAbsEigenvalueIndex = 1;
+        if (fabs(eigenvalues[2]) < fabs(eigenvalues[smallestAbsEigenvalueIndex]))
+          smallestAbsEigenvalueIndex = 2;
+
+        EigenvectorImageType::PixelType eigenvector;
+        typename EigenvectorImageType::PixelType::ValueType scale;
+        scale = eigenvectors[smallestAbsEigenvalueIndex][Self::ImageDimension-1] > 0 ? 1.0 : -1.0;
+        for (int i = 0; i < Self::ImageDimension; i++) {
+          eigenvector[i] = scale * eigenvectors[smallestAbsEigenvalueIndex][i];
+        }
+
+        oeit.Value() = eigenvector; // Make this the eigenvector
+      }
+      }
+    ++oit;
+    ++it;
+    if (m_GenerateScalesOutput)
+      {
+      ++osit;
+      }
+    if (m_GenerateHessianOutput)
+      {
+      ++ohit;
+      }
+    if (m_GenerateEigenvectorOutput)
+      {
+      ++oeit;
+      }
+    if (m_GenerateHessianOutput || m_GenerateEigenvectorOutput)
+      {
+      ++hit;
+      }
+    }
+}
+
+template <typename TInputImage,
+          typename THessianImage, 
+          typename TEigenvectorImage,
+          typename TOutputImage >
+double
+MultiScaleHessianBasedMeasureImageFilter2
+<TInputImage,THessianImage,TEigenvectorImage,TOutputImage>
+::ComputeSigmaValue(int scaleLevel)
+{
+  double sigmaValue;
+
+  if (m_NumberOfSigmaSteps < 2)
+    {
+    return m_SigmaMinimum;
+    }
+
+  switch (m_SigmaStepMethod)
+    {
+    case Self::EquispacedSigmaSteps:
+      {
+      const double stepSize = vnl_math_max(1e-10, ( m_SigmaMaximum - m_SigmaMinimum ) / (m_NumberOfSigmaSteps - 1));
+      sigmaValue = m_SigmaMinimum + stepSize * scaleLevel;
+      break;
+      }
+    case Self::LogarithmicSigmaSteps:
+      {
+      const double stepSize = vnl_math_max(1e-10, ( vcl_log(m_SigmaMaximum) - vcl_log(m_SigmaMinimum) ) / (m_NumberOfSigmaSteps - 1));
+      sigmaValue = vcl_exp( vcl_log (m_SigmaMinimum) + stepSize * scaleLevel);
+      break;
+      }
+    default:
+      throw ExceptionObject(__FILE__, __LINE__,"Invalid SigmaStepMethod.",ITK_LOCATION);
+      break;
+    }
+
+  return sigmaValue;
+}
+
+template <typename TInputImage,
+          typename THessianImage, 
+          typename TEigenvectorImage,
+          typename TOutputImage >
+void
+MultiScaleHessianBasedMeasureImageFilter2
+<TInputImage,THessianImage,TEigenvectorImage,TOutputImage>
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+  os << indent << "SigmaMinimum:  " << m_SigmaMinimum << std::endl;
+  os << indent << "SigmaMaximum:  " << m_SigmaMaximum  << std::endl;
+  os << indent << "NumberOfSigmaSteps:  " << m_NumberOfSigmaSteps  << std::endl;
+  os << indent << "SigmaStepMethod:  " << m_SigmaStepMethod  << std::endl;
+  os << indent << "HessianToMeasureFilter: " << m_HessianToMeasureFilter << std::endl;
+  os << indent << "GenerateScalesOutput: " << m_GenerateScalesOutput << std::endl;
+  os << indent << "GenerateHessianOutput: " << m_GenerateHessianOutput << std::endl;
+  os << indent << "GenerateEigenvectorOutput: " << m_GenerateEigenvectorOutput << std::endl;
+}
+
+
+} // end namespace itk
+
+#endif
---
+++ lib/filters/itk/itkMultiScaleHessianBasedMeasureImageFilter2.h
@@ -0,0 +1,211 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkMultiScaleHessianBasedMeasureImageFilter2.h,v $
+  Language:  C++
+  Date:      $Date: 2009/06/16 18:55:11 $
+  Version:   $Revision: 1.1 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkMultiScaleHessianBasedMeasureImageFilter2_h
+#define __itkMultiScaleHessianBasedMeasureImageFilter2_h
+
+#include "itkImageToImageFilter.h"
+#include "itkImage.h"
+#include "itkHessianRecursiveGaussianImageFilter.h"
+
+namespace itk
+{
+/**\class MultiScaleHessianBasedMeasureImageFilter2
+ * \brief A filter to enhance structures using Hessian eigensystem-based 
+ * measures in a multiscale framework
+ * 
+ * The filter evaluates a Hessian-based enhancement measure, such as vesselness 
+ * or objectness, at different scale levels. The Hessian-based measure is computed 
+ * from the Hessian image at each scale level and the best response is selected. 
+ *
+ * Minimum and maximum sigma value can be set using SetMinSigma and SetMaxSigma
+ * methods respectively. The number of scale levels is set using 
+ * SetNumberOfSigmaSteps method. Exponentially distributed scale levels are 
+ * computed within the bound set by the minimum and maximum sigma values 
+ * 
+ * The filter computes a second output image (accessed by the GetScalesOutput method)
+ * containing the scales at which each pixel gave the best reponse. 
+ *
+ * \author Luca Antiga Ph.D.  Medical Imaging Unit,
+ *                            Bioengineering Deparment, Mario Negri Institute, Italy.
+ *
+ * \sa HessianToObjectnessMeasureImageFilter 
+ * \sa Hessian3DToVesselnessMeasureImageFilter 
+ * \sa HessianSmoothed3DToVesselnessMeasureImageFilter 
+ * \sa HessianRecursiveGaussianImageFilter 
+ * \sa SymmetricEigenAnalysisImageFilter
+ * \sa SymmetricSecondRankTensor
+ * 
+ * \ingroup IntensityImageFilters TensorObjects
+ *
+ * NOTICE: This version includes support for generating an output image of the
+ * eigenvector associated with the minimum absolute eigenvalue of the Hessian -
+ * Cory Quammen
+ *
+ */
+template <typename TInputImage,
+          typename THessianImage, 
+          typename TEigenvectorImage,
+          typename TOutputImage=TInputImage >
+class ITK_EXPORT MultiScaleHessianBasedMeasureImageFilter2 
+: public
+ImageToImageFilter< TInputImage,TOutputImage > 
+{
+public:
+  /** Standard class typedefs. */
+  typedef MultiScaleHessianBasedMeasureImageFilter2          Self;
+  typedef ImageToImageFilter<TInputImage,TOutputImage>      Superclass;
+
+  typedef SmartPointer<Self>                                      Pointer;
+  typedef SmartPointer<const Self>                                ConstPointer;
+
+  typedef TInputImage                                    InputImageType;
+  typedef TOutputImage                                   OutputImageType;
+  typedef THessianImage                                  HessianImageType;
+  typedef TEigenvectorImage                              EigenvectorImageType;
+
+  typedef ImageToImageFilter< HessianImageType, OutputImageType  >  HessianToMeasureFilterType;
+
+  typedef typename TInputImage::PixelType                InputPixelType;
+  typedef typename TOutputImage::PixelType               OutputPixelType;
+
+  /** Image dimension. */
+  itkStaticConstMacro(ImageDimension, unsigned int, ::itk::GetImageDimension<InputImageType>::ImageDimension);
+ 
+  /** Hessian computation filter. */
+  typedef HessianRecursiveGaussianImageFilter< InputImageType, HessianImageType> HessianFilterType;
+ 
+  /** Update image buffer that holds the best objectness response */ 
+  typedef Image< double, itkGetStaticConstMacro(ImageDimension) > UpdateBufferType;
+  typedef typename UpdateBufferType::ValueType                    BufferValueType;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+  
+  /** Set/Get macros for SigmaMin */
+  itkSetMacro(SigmaMinimum, double);
+  itkGetMacro(SigmaMinimum, double);
+  
+  /** Set/Get macros for SigmaMax */
+  itkSetMacro(SigmaMaximum, double);
+  itkGetMacro(SigmaMaximum, double);
+
+  /** Set/Get macros for Number of Scales */
+  itkSetMacro(NumberOfSigmaSteps, int);
+  itkGetMacro(NumberOfSigmaSteps, int);
+
+  /** Set/Get HessianToMeasureFilter. This will be a filter that takes 
+   Hessian input image and produces enhanced output scalar image. The filter must derive from 
+   itk::ImageToImage filter */
+  itkSetObjectMacro( HessianToMeasureFilter, HessianToMeasureFilterType); 
+  itkGetObjectMacro( HessianToMeasureFilter, HessianToMeasureFilterType); 
+
+
+  typedef enum { EquispacedSigmaSteps = 0,
+                 LogarithmicSigmaSteps = 1 } SigmaStepMethodType;
+
+  /** Set/Get the method used to generate scale sequence (Equispaced
+   * or Logarithmic) */
+  itkSetMacro(SigmaStepMethod, SigmaStepMethodType);
+  itkGetMacro(SigmaStepMethod, SigmaStepMethodType);
+  void SetSigmaStepMethodToEquispaced()
+    { 
+    this->SetSigmaStepMethod(Self::EquispacedSigmaSteps);
+    }
+
+  void SetSigmaStepMethodToLogarithmic()
+    {
+    this->SetSigmaStepMethod(Self::LogarithmicSigmaSteps);
+    }
+
+  /** FIX ME: MOVE this to implementation file */
+  /** Get the image containing the Hessian computed at the best
+   * response scale */
+  const HessianImageType* GetHessianOutput() const
+    {
+    return static_cast<const HessianImageType*>(this->ProcessObject::GetOutput(2));
+    }
+
+  /** Get the eigenvector associated with eigenvalue of smallest absolute value
+   * computed at the best response scale */
+  const EigenvectorImageType* GetEigenvectorOutput() const
+    {
+    return static_cast<const EigenvectorImageType*>(this->ProcessObject::GetOutput(3));
+    }
+ 
+  /** Get the image containing the scales at which each pixel gave the
+   * best response */
+  const OutputImageType* GetScalesOutput() const
+    {
+    return static_cast<const OutputImageType*>(this->ProcessObject::GetOutput(1));
+    }
+
+  /** FIX ME : ADD DOCUMENTATION */
+  itkSetMacro(GenerateScalesOutput,bool);
+  itkGetMacro(GenerateScalesOutput,bool);
+  itkBooleanMacro(GenerateScalesOutput);
+
+  /** FIX ME : ADD DOCUMENTATION */
+  itkSetMacro(GenerateHessianOutput,bool);
+  itkGetMacro(GenerateHessianOutput,bool);
+  itkBooleanMacro(GenerateHessianOutput);
+
+  itkSetMacro(GenerateEigenvectorOutput,bool);
+  itkGetMacro(GenerateEigenvectorOutput,bool);
+  itkBooleanMacro(GenerateEigenvectorOutput);
+
+
+protected:
+  MultiScaleHessianBasedMeasureImageFilter2();
+  ~MultiScaleHessianBasedMeasureImageFilter2() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+  
+  /** Generate Data */
+  void GenerateData( void );
+
+private:
+  void UpdateMaximumResponse(double sigma);
+
+  double ComputeSigmaValue(int scaleLevel);
+  
+  void AllocateUpdateBuffer();
+
+  //purposely not implemented
+  MultiScaleHessianBasedMeasureImageFilter2(const Self&); 
+  void operator=(const Self&); //purposely not implemented
+
+  double                      m_SigmaMinimum;
+  double                      m_SigmaMaximum;
+
+  int                         m_NumberOfSigmaSteps;
+  SigmaStepMethodType         m_SigmaStepMethod;
+
+  typename HessianToMeasureFilterType::Pointer  m_HessianToMeasureFilter;
+  typename HessianFilterType::Pointer           m_HessianFilter;
+  typename UpdateBufferType::Pointer            m_UpdateBuffer;
+
+  bool m_GenerateScalesOutput;
+  bool m_GenerateHessianOutput;
+  bool m_GenerateEigenvectorOutput;
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkMultiScaleHessianBasedMeasureImageFilter2.cxx"
+#endif
+  
+#endif
