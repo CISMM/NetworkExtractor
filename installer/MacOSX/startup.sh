a62bfb0d464371f83e0f82e9b256709508e63ea2
Cory Quammen <cquammen@cs.unc.edu>
2009-06-30 20:30:50 +0000

    2009-06-30  Cory Quammen <cquammen@cs.unc.edu>
       * CMake/MacOSXPackage.txt: Added configuration of Info.plists file so
       that version number of program is incorporated in several keys.
    
       * Info.plist: Added tags to file that get replaced with program
       version number.
    
       * startup.sh: Modified regular expression in file to handle executable
       name with version number.

--- installer/MacOSX/startup.sh
+++ installer/MacOSX/startup.sh
@@ -6,7 +6,7 @@
 
 echo "Running FibrinAnalysis executable."
 
-FA_BUNDLE="`echo "$0" | sed -e 's/\/Contents\/MacOS\/FibrinAnalysis//'`"
+FA_BUNDLE="`echo "$0" | sed -e 's/\/Contents\/MacOS\/FibrinAnalysis\ [0-9]*.[0-9]*.[0-9]*//'`"
 FA_EXECUTABLE="$FA_BUNDLE/Contents/Resources/bin/FibrinAnalysis"
 
 export "DYLD_LIBRARY_PATH=$FA_BUNDLE/Contents/Resources/lib/"
